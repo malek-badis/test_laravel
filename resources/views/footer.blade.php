159f6dfe97616874878abb97a449dde49446cebf
badis malek <badismalek123@gmail.com>
2021-03-28 22:31:14 +0100

    header,footer and login page done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,13 @@
+<div class="card">
+	
+  <div class="card-header">
+    Citation
+  </div>
+  <div class="card-body">
+    <blockquote class="blockquote mb-0">
+      <p>«Une autre monnaie, fonctionnant sur d'autres bases, peut apporter une partie de la solution et changer en profondeur nos relations économiques et apporter de vraies réponses à la pauvreté.»</p>
+      <footer class="blockquote-footer">Philippe Herlin  <cite title="http://www.philippeherlin.com/la_fin_des_banques.htm"></cite></footer>
+    </blockquote>
+</div>
+  
+</div>
